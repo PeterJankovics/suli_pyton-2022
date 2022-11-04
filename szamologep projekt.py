2fc4772a41f3e8fd40e54724a924811d5c2446d8
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2022-11-04 14:02:49 +0100

    gyakorlas

---
+++ szamologep projekt.py
@@ -0,0 +1,17 @@
+szam1 = input("Első szám: ")
+szam2 = input("Második szám: ")
+muvelet = input("Művelet: ")
+if muvelet == "+":
+    print(int(szam1) + int(szam2))
+elif muvelet == "-":
+        print(int(szam1) - int(szam2))
+elif muvelet == "*":
+        print(int(szam1) * int(szam2))
+elif muvelet == "/":
+        print(int(szam1) / int(szam2))
+elif muvelet == "//":
+        print(int(szam1) // int(szam2))
+elif muvelet == "**":
+        print(int(szam1) ** int(szam2))
+elif muvelet == "%":
+        print(int(szam1) % int(szam2))
