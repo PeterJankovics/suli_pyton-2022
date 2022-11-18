199d8f5d15948517d27bec15f548cf3eebe935f4
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 14:04:53 +0100

    szambe

--- szambe.py
+++ szambe.py
@@ -3,10 +3,21 @@
 # 2022.11.18. Jankovics Péter
 
 def szamBe(szoveg, tort=False, minimum=False, maximum=False):
-    print(szoveg)
-    print(tort)
-
-szamBe("Aggyá meg számot: ",tort=True)
+#   print(szoveg)
+#   print(tort)
+#   print(minimum)
+    hiba=True
+    while hiba:
+        try:
+            if tort: 
+                szam=float(input(szoveg))
+            else:
+                szam=int(input(szoveg))    
+        except:
+            print("HIbas")
+        else:
+            hiba=False
+szamBe("Aggyá meg számot: ", minimum=10, tort=True)
 
     
     
