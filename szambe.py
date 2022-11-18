7dd96189712efa50f9aaed5e29e4ac44eca2aadf
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 13:20:28 +0100

    szambe

---
+++ szambe.py
@@ -0,0 +1,12 @@
+# szambekero modul
+# tobbfele parameterezessel
+# 2022.11.18. Jankovics Péter
+
+def szamBe(szoveg, tort=False, minimum=False, maximum=False):
+    print(szoveg)
+    print(tort)
+
+szamBe("Aggyá meg számot: ",tort=True)
+
+    
+    
