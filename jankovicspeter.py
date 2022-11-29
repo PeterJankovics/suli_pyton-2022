4e3966ce58dfe53f0bf2905cbc5e5c273771b22d
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2022-11-29 14:49:22 +0100

    felm meg

---
+++ jankovicspeter.py
@@ -0,0 +1,27 @@
+# 1
+szam = 1
+
+for szam in range(10):
+    szam = int(input("Kérek szamot: "))
+    print(szam)           
+# 2
+print(szam)
+
+
+
+# 5
+szoveg = "Integer eget pűharetra magna. Nulla ex urna, congue ac tincidunt ut, interdum et metus. Phasellus nunc nunc, consectetur eu odio vitae, ullamcorper sagittis nisi. Ut quam tortor, tempus sit amet diam nec, auctor iaculis leo. Donec ut libero velit. Maecenas nisi magna, congue ut tortor quis, maximus maximus arcu. Mauris et commodo nibh. Fusce id est vehicula, consectetur mi et, molestie sapien."
+print(szoveg)
+bek = input("kerek egy betut: ")
+print(szoveg.replace(bek))
+
+
+# 9
+a = "Integer eget pűharetra magna. Nulla ex urna, congue ac tinciduntut, interdum et metus. Phasellus nunc nunc, consectetur eu odio vitae, ullamcorper sagittis nisi. Ut quam tortor, tempus sit amet diam nec, auctor iaculis leo. Donec ut libero velit. Maecenas nisi magna, congue ut tortor quis, maximus maximus arcu. Mauris et commodo nibh. Fusce id est vehicula, consectetur mi et, molestie sapien."
+print(a.replace("."," "))
+print(a.replace(","," "))
+print(a.replace("!"," "))
+print(a.replace("?"," "))
+
+
+
