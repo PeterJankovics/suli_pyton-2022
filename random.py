f8350a01ae5c7f574b02715805eb13f07a6dccc5
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2022-12-08 13:18:42 +0100

    random

--- random.py
+++ random.py
@@ -12,3 +12,30 @@ lista = []
 for i in range(10):
     lista.append(random.randint(10,99))
 print(lista)
+
+print(random.randint(-1000,1000)*3)
+lista = []
+for i in range(100):
+      lista.append(random.randint(-1000,1000)*3)
+print(lista)      
+print(sum(lista))
+l5= []
+for e in lista:
+    if e % 5 == 0:
+        l5.append(e)
+        
+        
+print(l5)
+
+l5 = [e//15 for e in lista if e % 5 == 0]
+
+print(l5)
+
+
+
+
+
+
+
+
+
