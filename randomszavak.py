ec0526fffb42bab60d7423959375d83f63a3b599
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2022-12-13 14:47:05 +0100

    random szavak

--- randomszavak.py
+++ randomszavak.py
@@ -1,3 +1,4 @@
+
 import random
 szavak = ["alma","körte","banán","dinnye","barac",]
 # szandom szó kiirasa
@@ -5,3 +6,19 @@ szavak = ["alma","körte","banán","dinnye","barac",]
 print(szavak[random.randint(0,len(szavak)-1)])
 
 print(random.choice(szavak))
+
+print("-"*52)
+
+nagylista = []
+for e in szavak:
+    print(e)
+    kislista = []
+    kislista.append(e)
+    kislista.append(random.randint(12,312))
+    print(kislista)
+    nagylista.append(kislista)
+    
+print(nagylista)    
+
+for e in nagylista:
+    print(e[0].ljust(10), str(e[1]).rjust(4) , "kg")
