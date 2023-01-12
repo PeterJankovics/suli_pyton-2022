e4b5ca82e3d73d626f353ab2e9fd167427f87d5a
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-01-12 13:18:51 +0100

    file kezelés

--- file kezeles.py
+++ file kezeles.py
@@ -1,3 +1,19 @@
 f = open("proba.txt","w")
 f.write("helloka")
+f.write("\n")
+f.write("vilag")       
 f.close()
+
+f = open("proba.txt","r")
+szoveg = f.read()
+sorok = szoveg.split("\n")
+print(sorok)
+f.close()
+
+f = open("proba.txt","r")
+sorok = []
+for sor in f:
+    sorok.append(sor.replace("\n","").replace("\r",""))
+print(sorok)    
+f.close()
+
