0d4ab46124c34a65176c1f24c747081eff29862d
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-02-03 09:39:37 +0100

    szamok

--- erettsegi/szamok.py
+++ erettsegi/szamok.py
@@ -1,3 +1,5 @@
+import random
+
 f = open("felszam.txt","r")
 
 kerdesek = []
@@ -31,8 +33,58 @@ for e in kerdesek:
 print("A valaszok számértéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))    
 
 
+temakorok = [] 
+
+for e in kerdesek:
+    if e[3] not in temakorok:
+        temakorok.append(e[3])
+
+print("5. feladat")
+print("A témakörök: " + ", ".join(temakorok))      
 
 
+
+print("6. feladat:")
+
+beker = input("Milyen temakorbol szeretne kerdest kapni? ")
+
+
+
+ujlista = [e for e in kerdesek if e[3] == beker]
+
+sorsolt = random.choice(ujlista)
+
+valasz = input(sorsolt[0])
+if int(valasz) == sorsolt[1]:
+    print(str(sorsolt[2]) + " pont")
+else:
+    print("0 pont")
+    print("A helyes valasz: " + str(sorsolt[1]))
+
+print("7. feladat:")
+
+lista10 = []
+for i in range(10):
+    r = random.choice(kerdesek)
+    while r in lista10:
+        r = random.choice(kerdesek)
+    lista10.append(r)
+print(lista10)    
+
+
+
+random.shuffle(kerdesek)
+lista10 = kerdesek[0:10]
+print(lista10)
+
+f = open("tesztfel.txt","w")
+
+ossz = 0
+for e in lista10:
+    f.write(str(e[2]) + " " + str(e[1]) + " " + e[0] + "\n")
+    ossz += e[2]
+f.write("A feladatokra osszesen {0} pont adhato. ".format(ossz))
+f.close()
 
 
 
