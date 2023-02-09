f4560b86fa5d07454f06faf5e6427961a279042e
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-02-09 13:20:25 +0100

    tesztverseny

--- erettsegi/tesztverseny/tesztverseny.py
+++ erettsegi/tesztverseny/tesztverseny.py
@@ -1,3 +1,14 @@
+def pontszamit(valasz, helyes):
+    pont = 0
+    for sorszam, betu in enumerate (valasz):
+        if betu == helyes[sorszam]:
+            if sorszam < 5:
+                pont += 3
+            elif sorszam < 10:
+                pont += 4
+                
+
+            
 f = open("valaszok.txt","r")
 print("1. fladat:")
 adatok = f.read().split("\n")
@@ -40,8 +51,21 @@ for sorszam, betu in enumerate (versenyzovalasza):
         print(" ", end="")
         
 print("\t a versenyző helyes válaszai")
+print("5. feladat:")
+feladat = int(input("A feladat sorszáma = "))
+db = 0
 
+for e in valaszok:
+    if e[1][feladat] == helyes[feladat]:
+        db += 1
 
+print("A feladatra {0} fő, a versenyzők {1:.2%}-a adott helyes választ".format(db, db/len(valaszok)))        
+
+f = open("pontok.txt","w")
+
+for e in valaszok:
+    pont = pontszamit(e[1],helyes)
+f.close()
 
 
 
