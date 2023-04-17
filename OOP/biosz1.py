72065a21ae13ca92ae1d4ad4e8ee0c560e3b42dd
PeterJankovics <jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hu>
2023-04-17 18:10:23 +0200

    biosz1

--- OOP/biosz1.py
+++ OOP/biosz1.py
@@ -18,18 +18,70 @@ Frisch, 1775"""
 
 
 class orszag():
-    def __init__(self, orszag):
-        self.orszag = orszag
+    def __init__(self, o):
+        self.orszag = o
 class torzs(orszag):
-    def __init__(self, orszag, torzs):
-        super().__init__(orszag)
-        self.torzs = torzs
+    def __init__(self, o, t):
+        super().__init__(o)
+        self.torzs = t
 class altorzs(torzs):
     def __init_(self, o,t,at):
         super.__init__(o,t)
         self.altorzs = at
-class foosztaly(torzs):
-    def __init__(self, o, t, at):
-        super().__init__(o, t,at)
+class foosztaly(altorzs):
+    def __init__(self, o, t, at, fo):
+        super().__init__(o, t, at)
         self.foosztaly = fo
+class osztaly(foosztaly):
+    def __init__(self,o, t, at, fo, osz):
+        super().__init__(o, t, at, fo)
+        self.osztaly = osz
+class alosztaly(osztaly):
+    def __init__(self,o, t, at, fo, osz, al):
+        super().__init__(o, t, at, fo, osz)
+        self.altorzs = al
+class csoport(alosztaly):
+    def __init__(self,o, t, at, fo, osz, al, cso):
+        super().__init__(o, t, at, fo, osz, al)
+        self.csoport = cso        
+class alosztalysag(csoport):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz):
+        super().__init__(o, t, at, fo, osz, al, cso)
+        self.alosztaly = alosz  
+class oregrend(alosztalysag):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz)
+        self.oregrend = oreg
+class csoport2(oregrend):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg)
+        self.csoport2 = csop2
+class rend(csoport2):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg, csop2)
+        self.rend = r
+class alrend(rend):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, alr):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r)
+        self.alrend = al
+class csalad(alrend):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, alr, csal):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, al)
+        self.csalad = csal        
+class alcsalad(csalad):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, alr, csal, alcsal):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, al, csal)
+        self.alcsalad = alcsal
+class nemzetseg(alcsalad):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, alr, csal, alcsal, nemz):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, al, csal, alcsal)
+        self.nemzetseg = nemz 
+class nem(nemzetseg):
+    def __init__(self,o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, alr, csal, alcsal, nemz, nem):
+        super().__init__(o, t, at, fo, osz, al, cso, alosz, oreg, csop2, r, al, csal, alcsal)
+        self.nem = nem         
+
+
+
+
         
